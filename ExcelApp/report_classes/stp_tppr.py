5ae7a3dc76f5d2197a21000809004a08e27f6ba0
Schofield <Hunter.Schofield@york.ca>
2017-09-11 08:58:18 -0400

    added summary to payment report

--- ExcelApp/report_classes/stp_tppr.py
+++ ExcelApp/report_classes/stp_tppr.py
@@ -45,6 +45,7 @@ def render(res, params):
 
 	#MAIN DATA
 	cons = {}
+	summary = {}
 
 	for iid, val in enumerate(data["items"]):
 		if str(data["items"][iid]["contractyear"]) == year:
@@ -62,6 +63,14 @@ def render(res, params):
 					data["items"][iid].get("up"),
 					data["items"][iid].get("total")
 					])
+
+			if not data["items"][iid]["item"] in summary:
+				summary.update({data["items"][iid]["item"] : [
+					1,
+					data["items"][iid].get("up")
+					]})
+			else:
+				summary[data["items"][iid]["item"]][0] += 1
 				
 
 	for cid, con in enumerate(sorted(cons)):
@@ -81,13 +90,41 @@ def render(res, params):
 		cr += 2
 		start = cr
 		for contract in cons[con]:
-			worksheets[cid].write_row('A{}'.format(cr), contract, format_text)
+			worksheets[cid].write_row('A{}'.format(cr), [contract[0], contract[1]], format_text)
+			worksheets[cid].write_row('C{}'.format(cr), [contract[2], contract[3]], item_format_money)
 			cr += 1
 		worksheets[cid].write('A{}'.format(cr), "Subtotal: ", subtotal_format)
 		worksheets[cid].write_formula('B{}'.format(cr), "=SUM(B{}:B{})".format(start, cr - 1), subtotal_format)
-		worksheets[cid].write('C{}'.format(cr), " ", subtotal_format)
-		worksheets[cid].write_formula('D{}'.format(cr), "=SUM(D{}:D{})".format(start, cr - 1), subtotal_format)
+		worksheets[cid].write('C{}'.format(cr), " ", subtotal_format_money)
+		worksheets[cid].write_formula('D{}'.format(cr), "=SUM(D{}:D{})".format(start, cr - 1), subtotal_format_money)
 		cr += 2
+
+	worksheets.append(workbook.add_worksheet('Payment Summary'))
+
+	stp_config.const.write_gen_title(title, workbook, worksheets[-1], rightmost_idx, year, con_num)
+	worksheets[-1].insert_image('C1', stp_config.CONST.ENV_LOGO,{'x_offset':180,'y_offset':18, 'x_scale':0.5,'y_scale':0.5, 'positioning':2})
+
+	worksheets[-1].set_column('A:D', 45)
+	worksheets[-1].set_row(0,36)
+	worksheets[-1].set_row(1,36)
+
+	cr = 7
+	worksheets[-1].merge_range('A{}:D{}'.format(cr,cr), 'Summary', item_header_format)
+	worksheets[-1].write_row('A{}'.format(cr+1), item_fields, item_header_format)
+	cr += 2
+
+	for sid, sitem in enumerate(sorted(summary)):
+		d = [sitem]
+		d.extend(summary[sitem])
+		worksheets[-1].write_row('A{}'.format(cr), [d[0], d[1]], format_text)
+		worksheets[-1].write('C{}'.format(cr), d[2], item_format_money)
+		worksheets[-1].write_formula('D{}'.format(cr), '=B{}*C{}'.format(cr, cr), item_format_money)
+		cr += 1
+
+	worksheets[-1].write('A{}'.format(cr), 'Grand Total: ', subtotal_format)
+	worksheets[-1].write_formula('B{}'.format(cr), '=SUM(B9:B{})'.format(cr-1), subtotal_format)
+	worksheets[-1].write('C{}'.format(cr), ' ', subtotal_format)
+	worksheets[-1].write_formula('D{}'.format(cr), '=SUM(D9:D{})'.format(cr-1), subtotal_format_money)
 		
 	workbook.close()
 
