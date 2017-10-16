03a31cc9348f3571232eb7901392fbc089e09e05
Gary Kang <dragon_gary@msn.com>
2017-10-16 11:00:29 -0400

    config for prod done

--- PythonOfficePrint/urls.py
+++ PythonOfficePrint/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^stp/', include('stp.urls')),
-    url(r'^eRoadMapPrint/', include('eRoadMapPrint.urls'))
+    url(r'^eRoadMapPrint/', include('eRoadMapPrint.urls')),
     url(r'^admin/', admin.site.urls),
 ]
